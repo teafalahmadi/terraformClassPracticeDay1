f35c45cda3ac8a4108bbd2127be444abe30a0d84
Teaf Alahmadi <teafjaber@gmail.com>
2025-09-28 10:58:43 +0300

    Update main.tf

--- basic_terraform_workflow/main.tf
+++ basic_terraform_workflow/main.tf
@@ -7,14 +7,15 @@ provider "azurerm" {
 # Complete the following block:
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = var.
+  location = var.location
 }
 
 # complete the following block:
 resource "azurerm_container_registry" "acrok" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group
-  location            = azurerm_resource_group
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = var.acr_sku
   admin_enabled       = true
 }
+
